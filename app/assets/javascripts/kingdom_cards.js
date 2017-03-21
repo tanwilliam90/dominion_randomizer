7eaf7603993cae659dab652fcb7f371c38b3a522
William Tan <tanwilliam90@gmail.com>
2017-03-21 15:29:38 -0700

    you can now replace a card with the click of a button! (for now it only chooses adventurer)

--- app/assets/javascripts/kingdom_cards.js
+++ app/assets/javascripts/kingdom_cards.js
@@ -4,5 +4,17 @@ $(document).ready(function()
       //sets default sort to the first column, descending
       sortList: [[0,0]]
     });
+    $('.btn').on('click', function(){
+      $(this).closest('tr').remove();
+      $.ajax({
+        type: "GET",
+        url: "/kingdom_cards/"+card_id+".json",
+        dataType: 'json',
+        success: function(data){
+          var kingdom_card = data;
+          $('#cardList').append('<tr><td>'+kingdom_card.name+'</td><td>'+kingdom_card.set+'</td><td>'+kingdom_card.card_type+'</td><td>'+kingdom_card.sub_type+'</td><td>'+kingdom_card.cost+'<td></tr>');
+        }
+      });
+    })
   }
 );
