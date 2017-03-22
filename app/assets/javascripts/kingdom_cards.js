60c3c776e5130df880eefa948e3926b40f6bb950
William Tan <tanwilliam90@gmail.com>
2017-03-21 22:55:16 -0700

    reworked the way a card is replaced

--- app/assets/javascripts/kingdom_cards.js
+++ app/assets/javascripts/kingdom_cards.js
@@ -1,20 +1,46 @@
+function SelectNewCard(){
+  var me = this;
+  $.ajax({
+    type: "GET",
+    url: "/get_card",
+    dataType: 'json',
+    success: function(data){
+      var kingdom_card = data;
+      var new_tr = document.createElement('tr');
+      new_tr.setAttribute('data-card-id', kingdom_card.id);
+      var td_name = document.createElement('td');
+      td_name.innerHTML = kingdom_card.name;
+      var td_set = document.createElement('td');
+      td_set.innerHTML = kingdom_card.set;
+      var td_card_type = document.createElement('td');
+      td_card_type.innerHTML = kingdom_card.card_type;
+      var td_sub_type = document.createElement('td');
+      td_sub_type.innerHTML = kingdom_card.sub_type;
+      var td_cost = document.createElement('td');
+      td_cost.innerHTML = kingdom_card.cost;
+      var td_button = document.createElement('td');
+      var button = document.createElement('button');
+      button.setAttribute('class', "btn")
+      button.innerHTML = "Repick Card"
+      td_button.appendChild(button);
+      new_tr.appendChild(td_name);
+      new_tr.appendChild(td_set);
+      new_tr.appendChild(td_card_type);
+      new_tr.appendChild(td_sub_type);
+      new_tr.appendChild(td_cost);
+      new_tr.appendChild(td_button);
+      $(new_tr).on('click', 'button.btn', SelectNewCard);
+      $(me).parent().parent().replaceWith(new_tr);
+    }
+  })
+};
+
 $(document).ready(function()
   {
     $("#cardList").tablesorter({
       //sets default sort to the first column, descending
       sortList: [[0,0]]
     });
-    $('.btn').on('click', function(){
-      $(this).closest('tr').remove();
-      $.ajax({
-        type: "GET",
-        url: "/get_card",
-        dataType: 'json',
-        success: function(data){
-          var kingdom_card = data;
-          $('#cardList').append('<tr data-card-id="'+kingdom_card.id+'"><td>'+kingdom_card.name+'</td><td>'+kingdom_card.set+'</td><td>'+kingdom_card.card_type+'</td><td>'+kingdom_card.sub_type+'</td><td>'+kingdom_card.cost+'</td><td><button class="btn">Repick Card</button></td></tr>');
-        }
-      });
-    })
+    $('tr').on('click', 'button.btn', SelectNewCard)
   }
 );
